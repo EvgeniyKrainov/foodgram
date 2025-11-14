fbb75e844699044b2424828a009333aefcc6916d
Evgeniy Krainov <evgeniy_kraynov@bk.ru>
2025-11-14 16:11:32 +0300

    14.11 frontend ver 1.2

--- frontend/src/setupTests.js
+++ frontend/src/setupTests.js
@@ -3,3 +3,10 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+
+// Polyfill for clearImmediate
+if (typeof clearImmediate === 'undefined') {
+  global.clearImmediate = (timerId) => {
+    clearTimeout(timerId);
+  };
+}
