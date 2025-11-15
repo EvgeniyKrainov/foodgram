c33ce79cbcf29abdfdee1a96d672cf182afdb098
Evgeniy Krainov <evgeniy_kraynov@bk.ru>
2025-11-15 08:47:50 +0300

    15.11 deploy ver 1.5

--- apps/users/backends.py
+++ apps/users/backends.py
@@ -11,7 +11,7 @@ class EmailOrUsernameModelBackend(ModelBackend):
     def authenticate(self, request, username=None, password=None, **kwargs):
         if username is None:
             username = kwargs.get(User.USERNAME_FIELD)
-        
+
         try:
             user = User.objects.get(
                 Q(username__iexact=username) | 
@@ -19,7 +19,7 @@ class EmailOrUsernameModelBackend(ModelBackend):
             )
         except User.DoesNotExist:
             return None
-        
+
         if user.check_password(password) and self.user_can_authenticate(user):
             return user
         return None
