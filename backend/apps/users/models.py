320a407d00e72a6a99716db4d197e64815935d59
Evgeniy Krainov <evgeniy_kraynov@bk.ru>
2025-11-19 20:35:23 +0300

    19.11 avatar ver 1.0

--- backend/apps/users/models.py
+++ backend/apps/users/models.py
@@ -4,6 +4,12 @@ from django.db import models
 
 class User(AbstractUser):
     email = models.EmailField(max_length=254, unique=True)
+    avatar = models.ImageField(
+        upload_to='users/avatars/',
+        blank=True,
+        null=True,
+        verbose_name="Аватар"
+    )
 
     class Meta:
         ordering = ["id"]
