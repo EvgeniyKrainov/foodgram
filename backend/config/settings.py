de7514f5b71fbe3ad108c427ef92f2a7c1776f8e
Evgeniy Krainov <evgeniy_kraynov@bk.ru>
2025-11-23 13:23:44 +0300

    23.11 deploy ver 1.3

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -171,9 +171,8 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication',
     ],
 
-    'DEFAULT_PAGINATION_CLASS': [
+    'DEFAULT_PAGINATION_CLASS':
         'rest_framework.pagination.PageNumberPagination',
-    ],
 
     'PAGE_SIZE': DEFAULT_PAGE_SIZE,
 }
