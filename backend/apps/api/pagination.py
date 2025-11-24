ae75d8f31c1c0c665b4f47b071cd5f5c79d9f923
Evgeniy Krainov <evgeniy_kraynov@bk.ru>
2025-11-24 20:05:46 +0300

    24.11 success test api + deploy ver 1.7

--- backend/apps/api/pagination.py
+++ backend/apps/api/pagination.py
@@ -1,18 +1,9 @@
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
 
-from config.constants import DEFAULT_PAGE_SIZE, MAX_PAGE_SIZE
+from config.constants import DEFAULT_PAGE_SIZE
 
 
 class FoodgramPagination(PageNumberPagination):
     page_size = DEFAULT_PAGE_SIZE
     page_size_query_param = 'limit'
-    max_page_size = MAX_PAGE_SIZE
-
-    def get_paginated_response(self, data):
-        return Response({
-            'count': self.page.paginator.count,
-            'next': self.get_next_link(),
-            'previous': self.get_previous_link(),
-            'results': data
-        })
